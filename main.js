fb1d10ec49a5c6b67f4c40bd3e602625f4a967a9
nguyenvanbao <bao19042004@gmail.com>
2022-01-07 22:26:13 +0700

    day3

---
+++ main.js
@@ -0,0 +1,18 @@
+var openModal = document.querySelector('.open-modal-btn')
+var Modal = document.querySelector('.modal')
+var closeModal = document.querySelector('.modal-header i')
+var btnClose = document.querySelector('.modal-footer button')
+
+var open = function(e){
+    Modal.classList.toggle('hiden')
+}
+openModal.addEventListener('click',open)
+closeModal.addEventListener('click',open)
+btnClose.addEventListener('click',open)
+
+Modal.addEventListener('click',function(e){
+        if(e.target==e.currentTarget){
+            open()
+        
+        }
+})
